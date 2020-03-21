590f9eb43af983038341035f8d6e170fe19f71d2
Ali <mazg1493@gmail.com>
2020-03-22 02:27:28 +0600

    Added basic user model.

--- durgo/settings.py
+++ durgo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'durgo.apm',
+    'durgo.users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.User'
